a9fb95e8734ca60979272936f63b475d6405886a
GabrielSilvaRodrigues1 <gabrielsilvagithub@gmail.com>
2025-06-13 22:39:15 +0000

    Update

--- Aula_02/src/App.java
+++ Aula_02/src/App.java
@@ -10,7 +10,14 @@ public class App {
         String nome = sc.nextLine();
         System.out.println("Idade: ");
         int idade = sc.nextInt();
-        System.out.printf("Nome: %s \n Idade: %s. \n", nome, idade);
+        System.out.printf("Nome: %s \nIdade: %s \n", nome, idade);
         var tipoIndefinido = MENSAGEM_ERRO;
+        var numero = sc.nextInt();
+        //raiz quadrada;
+        System.out.printf("Raíz: %s \n", Math.sqrt(numero));
+        var bnr = Integer.toBinaryString(numero);
+        var vlr = numero | idade;
+        System.out.printf("Binario: %s \n, numero | idade = s%", bnr, vlr);
+
     }
 }
